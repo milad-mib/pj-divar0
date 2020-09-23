4a3b850504b5d99a5161b6129ca8641cc9fdfda0
milad-mib <milad0228@gmail.com>
2020-09-23 16:06:39 +0330

    import home.views & login_views

--- Divar/account/views.py
+++ Divar/account/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
+def login_view (request,):
+    return HttpResponse('WelCome to login_view')
