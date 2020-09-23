0c43203dc0aede39756c58a6acad006a4fdb4960
milad-mib <milad0228@gmail.com>
2020-09-23 15:55:33 +0330

    install Apps (account & Home) setting

--- Divar/Divar/settings.py
+++ Divar/Divar/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accunt.apps.AccountConfig',
+    'Home.apps.HomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
