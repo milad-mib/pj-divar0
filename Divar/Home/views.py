f1ee66cca795164568b76dab51748289c429aac7
milad-mib <milad0228@gmail.com>
2020-09-26 18:13:31 +0330

    conect link slug to html

--- Divar/Home/views.py
+++ Divar/Home/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse,reverse
 from . import models
 # Create your views here.
+# namayesh kool agahi
 def Home (request,):
     show_ads = models.Divar.objects.all().order_by('-Date')
     return render(request, 'Home/home.html', {'show_ad':show_ads})
+# namayesh link entekhabi
+def detailshow (request,slug):
+    return HttpResponse (slug)
